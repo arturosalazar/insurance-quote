bf3eb703962ab782e84aa50c6dd7f96cfd9ddcb3
Anna Shklover <ashklover@hawk.iit.edu>
2021-05-08 20:14:45 -0500

    comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { User } from './model/user';
 
+// causes errors
 /*
-var models = require("../../models"); //place on top of the file
+var models = require("./models"); //place on top of the file
 models.sequelize.sync().then(function() {
  var server = app.listen(app.get('port'), function() {
  console.log('Express server listening on port ' + server.address().port);
